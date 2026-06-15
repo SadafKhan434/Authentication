65109339458670d38c3406ab8469e255ca24786d
SadafKhan434 <sadafkhan2192@gmail.com>
2026-06-15 19:40:19 +0530

    token

--- src/Store/auth-context.js
+++ src/Store/auth-context.js
@@ -6,14 +6,17 @@ login:(token)=>{},
     logout:()=>{}
 })
    export  const AuthContextProvider=(props)=>{
-        const [token,setToken]=useState(null)
+    const initialToken= localStorage.getItem('token')
+        const [token,setToken]=useState(initialToken)
         const UserIsLoggedIn = !!token
 
         const loginHandler=(token)=>{
             setToken(token)
+            localStorage.setItem('token',token)
         }
         const logoutHandler=()=>{
             setToken(null)
+            localStorage.removeItem('token')
         }
         const contextValue={
             token:token,
